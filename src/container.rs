0026d584af9417f6cbd99e753fbcbe858abda649
Martin <A>
2024-05-24 01:08:58 -0400

    Add function opendir, readdir, and create in container.rs.

--- src/container.rs
+++ src/container.rs
@@ -1,13 +1,17 @@
 use anyhow::{bail, Ok, Result};
-use bincode::Options;
+use fuser::{FileType, ReplyDirectory};
 use serde::{Deserialize, Serialize};
+use std::ffi::OsStr;
 use std::fs::OpenOptions;
 use std::io::{Read, Seek, SeekFrom};
 use std::mem::size_of;
 use std::path::Path;
+use std::str::FromStr;
 use std::{fs::File, io::Write};
 
-use crate::sector::{self, EmptySector, FileData, FileMetadata, Sector};
+use crate::sector::{self, DirData, EmptySector, FileMetadata, Sector};
+
+use sector::FILE_NAME_SIZE;
 
 pub struct Container {
     container_name: String,
@@ -37,7 +41,7 @@ impl Container {
                 last_empty_sector: None,
                 next_ino: 2,
             };
-            let first_sector = Sector::FileMetadata(FileMetadata::new(1, None));
+            let first_sector = Sector::DirMetadata(FileMetadata::new(1, None));
 
             let mut buff = Vec::with_capacity(size_of::<ContainerMetadata>());
             bincode::serialize_into(&mut buff, &metadata)?;
@@ -155,22 +159,202 @@ impl Container {
         Ok(1)
     }
     fn get_empty_sector(&mut self) -> Result<u64> {
-        if let Some(empty_sector) = self.metadata.first_empty_sector {
-            return Ok(empty_sector);
+        if let Some(empty_sector_id) = self.metadata.first_empty_sector {
+            let Sector::Empty(empty_sector_data) = self.read_sector(empty_sector_id)? else {
+                bail!("Empty sector is not true empty sector");
+            };
+            if self.metadata.last_empty_sector == self.metadata.first_empty_sector {
+                self.metadata.first_empty_sector = None;
+                self.metadata.last_empty_sector = None;
+            } else {
+                self.metadata.first_empty_sector = empty_sector_data.next();
+            }
+            self.write_metadata()?;
+            return Ok(empty_sector_id);
         }
         self.append_empty_sector()?;
-        if let Some(empty_sector) = self.metadata.first_empty_sector {
-            return Ok(empty_sector);
+        if let Some(empty_sector_id) = self.metadata.first_empty_sector {
+            let Sector::Empty(empty_sector_data) = self.read_sector(empty_sector_id)? else {
+                bail!("Empty sector is not true empty sector");
+            };
+            if self.metadata.last_empty_sector == self.metadata.first_empty_sector {
+                self.metadata.first_empty_sector = None;
+                self.metadata.last_empty_sector = None;
+            } else {
+                self.metadata.first_empty_sector = empty_sector_data.next();
+            }
+            self.write_metadata()?;
+            return Ok(empty_sector_id);
         } else {
             bail!("No empty sector available");
         }
+    }
+    fn get_empty_entry(&mut self, dir_metadata: &FileMetadata) -> Result<Option<(u64, usize)>> {
+        let mut next_sector = dir_metadata.first_sector();
+
+        //Iterate through all sector of directory
+        while let Some(sector_id) = next_sector {
+            let base_sector = self.read_sector(sector_id)?;
+            let Sector::DirData(sector) = &base_sector else {
+                bail!(
+                    "Directory sector is not DirData (inode {}, sector {sector_id})",
+                    dir_metadata.ino()
+                );
+            };
+            //Look for an empty entry
+            for (i, entry) in sector.entries().iter().enumerate() {
+                if entry.empty {
+                    //Write the entry
+                    return Ok(Some((sector_id, i)));
+                }
+            }
+            next_sector = sector.next_sector();
+        }
+        Ok(None)
+    }
+    fn find_ino_sector(&mut self, ino: u64) -> Result<(u64, Sector)> {
+        for i in 0..self.metadata.sector_count {
+            let sector = self.read_sector(i)?;
+            if let Sector::DirMetadata(ref dir_metadata) = sector {
+                if dir_metadata.ino() == ino {
+                    return Ok((i, sector));
+                }
+            } else if let Sector::FileMetadata(ref file_metadata) = sector {
+                if file_metadata.ino() == ino {
+                    return Ok((i, sector));
+                }
+            }
+        }
+        bail!("Inode {ino} not found");
+    }
+    fn new_inode(&mut self) -> Result<u64> {
+        if self.metadata.next_ino == u64::max_value() {
+            bail!("No more inode");
+        }
+        self.metadata.next_ino += 1;
+        Ok(self.metadata.next_ino - 1)
+    }
+
+    pub fn opendir(&mut self, ino: u64) -> Result<u64> {
+        let (sector_id, sector) = self.find_ino_sector(ino)?;
+        Ok(1)
+    }
+    pub fn readdir(
+        &mut self,
+        ino: u64,
+        _fh: u64,
+        offset: i64,
+        reply: &mut ReplyDirectory,
+    ) -> Result<()> {
+        let mut index = 0;
+
+        // eprintln!("Look for ino {ino}");
+        let (_sector_id, sector) = self.find_ino_sector(ino)?;
+        let Sector::DirMetadata(dir_metadata) = sector else {
+            bail!("Inode {ino} is not a directory.");
+        };
+        let mut next_sector = dir_metadata.first_sector();
+
+        //Iterate through all sector of directory
+        while let Some(sector_id) = next_sector {
+            let base_sector = self.read_sector(sector_id)?;
+            let Sector::DirData(sector) = base_sector else {
+                eprintln!("{base_sector:?}");
+                bail!("Directory sector is not DirData (inode {ino}, sector {sector_id})");
+            };
+            for entry in sector.entries() {
+                if entry.empty {
+                    continue;
+                }
+                if index >= offset {
+                    let filetype = match entry.filetype {
+                        sector::FileType::Regular => FileType::RegularFile,
+                        sector::FileType::Directory => FileType::Directory,
+                    };
+                    if reply.add(entry.ino, index, filetype, entry.name.to_string()) {
+                        return Ok(());
+                    }
+                }
+                index += 1;
+            }
+            next_sector = sector.next_sector();
+        }
+
+        Ok(())
+    }
+    pub fn create(&mut self, parent: u64, name: &OsStr) -> Result<u64> {
+        let Some(name) = name.to_str() else {
+            bail!("Invalid name");
+        };
+        if name.len() >= FILE_NAME_SIZE {
+            bail!("Name is too long (max {FILE_NAME_SIZE})");
+        }
+        let (metadata_sector_id, mut metadata_sector) = self.find_ino_sector(parent)?;
+        let Sector::DirMetadata(dir_metadata) = &mut metadata_sector else {
+            bail!("Inode {parent} is not a directory.");
+        };
+        let new_inode = self.new_inode()?;
+        let empty_sector_id_file_metadata = self.get_empty_sector()?;
+        //TODO check if name already exist
+        let findings = self.get_empty_entry(dir_metadata)?;
+
+        let (sector_id, idx) = if let Some(a) = findings {
+            a
+        } else {
+            //append_empty_sector
+            let empty_sector_id = self.get_empty_sector()?;
+            eprintln!("Dir Data sector {empty_sector_id}");
+            //set the sector data
+            let mut sector = DirData::new();
+            if let Some(previous_id) = dir_metadata.first_sector() {
+                sector.set_previous(previous_id);
+            }
+            self.write_sector(empty_sector_id, &Sector::DirData(sector))?;
+            //modify metadata to emplace it at the front of the sector list
+            dir_metadata.set_first_sector(empty_sector_id);
+            dir_metadata.increase_length_sector();
+            self.write_sector(metadata_sector_id, &metadata_sector)?;
+            (empty_sector_id, 0)
+        };
+
+        //Read the sector in memory
+        let mut base_sector = self.read_sector(sector_id)?;
+        let Sector::DirData(sector) = &mut base_sector else {
+            bail!("Directory sector is not DirData (inode {parent}, sector {sector_id})");
+        };
+
+        //Should always be valid because it should have failed earlier otherwise (no new empty sector)
+        let entry = sector.entries_mut().get_mut(idx).unwrap();
+
+        //Write the entry
+        entry.ino = new_inode;
+        entry.empty = false;
+        let std::result::Result::Ok(heapless_name) =
+            heapless::String::<FILE_NAME_SIZE>::from_str(name)
+        else {
+            bail!("Error heapless::String::from_str for the filename.");
+        };
+        entry.name = heapless_name;
+        entry.filetype = sector::FileType::Regular;
+
+        eprintln!("base_sector {sector:?} - {}", sector.entries().len());
+        //Write to container
+        self.write_sector(sector_id, &base_sector)?;
+
+        // write metadata of new file
+        let empty_sector_id = empty_sector_id_file_metadata;
+        let sector = FileMetadata::new(new_inode, Some(parent));
+        self.write_sector(empty_sector_id, &Sector::FileMetadata(sector))?;
+        eprintln!("File metadata sector {empty_sector_id}");
+
+        Ok(new_inode)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::fs::remove_file;
     use super::*;
+    use std::fs::remove_file;
 
     #[test]
     fn append_empty_sector() {
@@ -178,7 +362,7 @@ mod tests {
         let mut container = Container::new("/tmp/canard".to_string()).unwrap();
         let sector_count = container.metadata.sector_count;
         container.append_empty_sector().unwrap();
-        assert_eq!(container.metadata.sector_count, sector_count+1);
+        assert_eq!(container.metadata.sector_count, sector_count + 1);
         assert_eq!(container.metadata.last_empty_sector, Some(sector_count));
         remove_file("/tmp/canard").unwrap();
     }
