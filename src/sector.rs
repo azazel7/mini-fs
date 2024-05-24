bd54ca848237799e0ea9d312bfac51d2f6ac8dcb
Martin <A>
2024-05-24 01:04:31 -0400

    Update sector structures with impl.

--- src/sector.rs
+++ src/sector.rs
@@ -10,9 +10,9 @@ pub enum Sector {
     DirMetadata(FileMetadata),
     DirData(DirData),
 }
-const DATA_CHUNK_SIZE: usize = 200;
-const FILE_NAME_SIZE: usize = 30;
-const DIR_SECTOR_SIZE: usize = 5;
+pub const DATA_CHUNK_SIZE: usize = 200;
+pub const FILE_NAME_SIZE: usize = 30;
+pub const DIR_SECTOR_SIZE: usize = 5;
 
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct EmptySector {
@@ -51,6 +51,21 @@ impl FileMetadata {
             first_sector: None,
         }
     }
+    pub fn ino(&self) -> u64 {
+        self.ino
+    }
+    pub fn parent(&self) -> Option<u64> {
+        self.parent
+    }
+    pub fn first_sector(&self) -> Option<u64> {
+        self.first_sector
+    }
+    pub fn set_first_sector(&mut self, sector_id : u64) {
+        self.first_sector = Some(sector_id);
+    }
+    pub fn increase_length_sector(&mut self) {
+        self.length_sector += 1;
+    }
 }
 #[serde_as]
 #[derive(Serialize, Deserialize, Debug)]
@@ -72,20 +87,57 @@ impl FileData {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Copy, Clone)]
 pub enum FileType {
     Regular,
     Directory,
 }
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct DirEntry {
-    ino: u64,
-    name: String<FILE_NAME_SIZE>,
-    filetype: FileType,
+    pub ino: u64,
+    pub name: String<FILE_NAME_SIZE>,
+    pub filetype: FileType,
+    pub empty: bool,
+}
+impl DirEntry {
+    pub fn empty() -> Self {
+        Self {
+            ino: 0,
+            name: String::new(),
+            filetype: FileType::Regular,
+            empty: true,
+        }
+    }
 }
 #[derive(Serialize, Deserialize, Debug)]
 pub struct DirData {
-    next_sector: u64,
-    previous_sector: u64,
+    next_sector: Option<u64>,
+    previous_sector: Option<u64>,
     files: Vec<DirEntry, DIR_SECTOR_SIZE>,
+}
+impl DirData {
+    pub fn new() -> Self {
+        let mut files = Vec::new();
+        let _ = files.resize(DIR_SECTOR_SIZE, DirEntry::empty());
+        DirData {
+            next_sector: None,
+            previous_sector: None,
+            files,
+        }
+    }
+    pub fn set_previous(&mut self, previous: u64) {
+        self.previous_sector = Some(previous);
+    }
+    pub fn set_next(&mut self, next: u64) {
+        self.next_sector = Some(next);
+    }
+    pub fn next_sector(&self) -> Option<u64> {
+        self.next_sector
+    }
+    pub fn entries(&self) -> &Vec<DirEntry, DIR_SECTOR_SIZE> {
+        &self.files
+    }
+    pub fn entries_mut(&mut self) -> &mut Vec<DirEntry, DIR_SECTOR_SIZE> {
+        &mut self.files
+    }
 }
