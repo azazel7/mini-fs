d6fa0f0cf45ad25caa47a392da417b1461fa3693
Martin <A>
2024-05-28 16:36:49 -0400

    Simplify comment parameters. Remove env_logger.

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,24 @@
-use clap::{crate_version, Arg, ArgAction, Command};
+use clap::Parser;
 use fuser::MountOption;
 use anyhow::{Context, Result};
 
 use mini_fs::{fuse_interface::FuseFs, logger::Logger};
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    mountpoint : String,
+    container : String,
+    #[arg(short = 'n', long)]
+    allow_notification : bool,
+}
+
 fn main() -> Result<()>{
     let appname = "mini-fs";
-    let matches = Command::new(appname)
-        .version(crate_version!())
-        .author("Martin")
-        .arg(
-            Arg::new("MOUNT_POINT")
-                .required(true)
-                .index(1)
-                .help("Act as a client, and mount FUSE at given path"),
-        )
-        .arg(
-            Arg::new("CONTAINER")
-                .required(true)
-                .index(2)
-                .help("The file that will contain everything"),
-        )
-        .arg(
-            Arg::new("allow-notification")
-                .long("allow-notification")
-                .short('n')
-                .action(ArgAction::SetTrue)
-                .help("Activate desktop notifications"),
-        )
-        .get_matches();
-    env_logger::init();
-    let mountpoint = matches.get_one::<String>("MOUNT_POINT").context("No mount point")?;
-    let container_name = matches.get_one::<String>("CONTAINER").context("No container file")?;
+    let cli = Cli::parse();
     let options = vec![MountOption::RW, MountOption::FSName(appname.to_string())];
-    let logger = Logger::new(appname.to_string(), matches.get_flag("allow-notification"));
-    let fuse_fs = FuseFs::new(container_name.to_string(), logger)?;
-    fuser::mount2(fuse_fs, mountpoint, &options).context("fuser::mount2 ")?;
+    let logger = Logger::new(appname.to_string(), cli.allow_notification);
+    let fuse_fs = FuseFs::new(cli.container, logger)?;
+    fuser::mount2(fuse_fs, cli.mountpoint, &options).context("fuser::mount2 ")?;
     Ok(())
 }
